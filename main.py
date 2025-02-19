453d69e7ec531837284d2e4bf72dc20e94c6e5bd
alex <106116143+maslov-cool@users.noreply.github.com>
2025-02-19 14:16:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import sys
 
 import pygame
 import requests
+import math
+
+
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000  # 111 километров в метрах
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    # Вычисляем расстояние между точками.
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
 
 
 def main(geocode):
@@ -37,5 +57,6 @@ A = []
 for i in [input('Адрес дома '), input('Адрес школы ')]:
     A.append(main(i))
 for i in range(1, len(A)):
-    s += ((float(A[i][0]) - float(A[i - 1][0])) ** 2 + (float(A[i][1]) - float(A[i - 1][1])) ** 2) ** 0.5 * 111
-print(f'путь составляет {s} км')
+    s += lonlat_distance((float(A[i][0]), float(A[i][1])), (float(A[i - 1][0]), float(A[i - 1][1])))
+print(f'путь составляет {s / 1000} км')
+
